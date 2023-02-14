f5347a719c06cc17841718e2a4b78760efe647c7
imsaifun <imsaifun@gmail.com>
2023-02-14 17:23:19 +0600

    initial

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+
+export default function Home() {
+    return (
+        <>
+            <div className="container">
+                <div className="item" style={{ height: 140 }} />
+                <div className="item" style={{ height: 190 }} />
+                <div className="item" style={{ height: 170 }} />
+                <div className="item" style={{ height: 120 }} />
+                <div className="item" style={{ height: 160 }} />
+                <div className="item" style={{ height: 180 }} />
+                <div className="item" style={{ height: 140 }} />
+                <div className="item" style={{ height: 150 }} />
+                <div className="item" style={{ height: 170 }} />
+                <div className="item" style={{ height: 170 }} />
+                <div className="item" style={{ height: 140 }} />
+                <div className="item" style={{ height: 190 }} />
+                <div className="item" style={{ height: 170 }} />
+                <div className="item" style={{ height: 120 }} />
+                <div className="item" style={{ height: 160 }} />
+                <div className="item" style={{ height: 180 }} />
+                <div className="item" style={{ height: 140 }} />
+                <div className="item" style={{ height: 150 }} />
+                <div className="item" style={{ height: 170 }} />
+                <div className="item" style={{ height: 170 }} />
+                <span className="item break" />
+                <span className="item break" />
+                <span className="item break" />
+            </div>
+
+        </>
+    )
+}
